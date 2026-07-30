38e562df1928b6840a4050c622f0bd0f8336852d
David Nelson <kd22707@gmail.com>
2026-07-29 19:59:14 -0500

    Fix spelling of forty

--- Calculator.py
+++ Calculator.py
@@ -109,7 +109,7 @@ if __name__ == "__main__":
     word_to_num = {'zero': 0, 'one': 1, 'two': 2, 'three': 3, 'four': 4, 'five': 5, 'six': 6, 'seven': 7, 'eight': 8, 'nine': 9, 'ten': 10, 'eleven': 11, 'twelve': 12, 'thirteen': 13, 'fourteen': 14, 'fifteen': 15, 'sixteen': 16, 'seventeen': 17, 'eighteen': 18, 'nineteen': 19}
     neg_signs = {'minus' : '-', 'negative' : '-'}    
     ones = {'one': 1, 'two': 2, 'three': 3, 'four': 4, 'five': 5, 'six': 6, 'seven': 7, 'eight': 8, 'nine': 9}
-    tens = {'twenty' : 20, 'thirty' : 30,'fourty' : 40, 'fifty' : 50, 'sixty' : 60, 'seventy' : 70, 'eighty' : 80, 'ninety' : 90}
+    tens = {'twenty' : 20, 'thirty' : 30,'forty' : 40, 'fifty' : 50, 'sixty' : 60, 'seventy' : 70, 'eighty' : 80, 'ninety' : 90}
     
 
     while True:
